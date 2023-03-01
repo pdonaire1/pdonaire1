7bd2f08f0f4e6611e243fc23a6fa61b0b69aea9a
Pablo González <pdonaire1@gmail.com>
2023-03-01 19:47:30 -0400

    Update page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,10 +5,10 @@ class Header extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function(network){
         return <li key={network.name}><a target="_blank" href={network.url}><i className={network.className}></i></a></li>
       })
     }
